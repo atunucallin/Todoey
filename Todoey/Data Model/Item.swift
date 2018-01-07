181198166be599a13a2faa8340cfe085cc6b9c3b
Dipanjan Mondal <atunucallin@gmail.com>
2018-01-07 17:27:55 +0530

    converted to mvc design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dipanjan Mondal on 07/01/2018.
+//  Copyright © 2018 Dipanjan Mondal. All rights reserved.
+//
+
+import Foundation
+
+class  Item {
+    var  title: String = ""
+    var done: Bool = false
+    
+}
